2c4f758171c56bc900496232ac33c84be9592441
CNwante <myphilothea@gmail.com>
2022-12-11 17:29:41 -0800

    The alphabet game Task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Outputs alphabets.
+ *
+ * Description: Outputs alphabets in lowercase.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 223; i++)
+		putchar(i);
+
+	return (0);
+}
