d5d62e2162b120590d3b72edf999cce644bad1a3
CNwante <myphilothea@gmail.com>
2022-12-22 18:24:47 -0800

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,13 +18,15 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		count1++;
 	}
-	do {
+
+	while (count2 < n)
+	{
 		*(dest + count1) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
 		count1++;
 		count2++;
-	} while (count2 < n);
+	}
 
 	return (dest);
 }
