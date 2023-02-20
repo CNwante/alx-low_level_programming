118dcdf1da16e36007ada53d0f6df85458ebff29
CNwante <myphilothea@gmail.com>
2023-02-20 06:46:30 -0800

    Task(0): check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: int data type parameter
+ *
+ * Return: 1 if c is uppercase,
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
